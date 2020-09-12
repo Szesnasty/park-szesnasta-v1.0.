352dd947e336400077546369a4ed638e57c59749
Pan Szesnasty <lukasz.jakubowski1993@gmail.com>
2020-09-12 15:55:12 +0200

    const in utilities and create routes const

---
+++ libs/utilities/src/lib/utilities/constant/routes.ts
@@ -0,0 +1,7 @@
+export const Routes = {
+  Home: {
+    path: '/',
+    name: 'home',
+    viewId: 'HOME',
+  },
+};
--- libs/utilities/src/lib/utilities/router-manager/router-manager.tsx
+++ libs/utilities/src/lib/utilities/router-manager/router-manager.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Users } from '@park-szesnasta/users';
 import { Route, Switch } from 'react-router-dom';
+import { Routes } from './../constant/routes';
 
 export const RouterManager = () => {
   return (
     <Switch>
-      <Route path={'/'} exact component={Users} />
+      <Route path={Routes.Home.path} exact component={Users} />
     </Switch>
   );
 };
