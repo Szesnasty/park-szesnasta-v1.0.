8971b5f1fe457809f0f768e618e477629e0c6868
Pan Szesnasty <lukasz.jakubowski1993@gmail.com>
2020-09-30 19:08:57 +0200

    simple loggedIn const in router-manager

--- apps/park-szesnasta/src/app/router-manager/router-manager.tsx
+++ apps/park-szesnasta/src/app/router-manager/router-manager.tsx
@@ -2,18 +2,36 @@ import React from 'react';
 import { UsersManagement } from '@park-szesnasta/users-management';
 import { Login } from '@park-szesnasta/login';
 import { Route, Switch } from 'react-router-dom';
+import { Redirect } from 'react-router';
 import { Routes } from '@park-szesnasta/utilities';
 
 export const RouterManager = () => {
+  const loggedIn = true;
+
   return (
     <Switch>
-      <Route path={Routes.Home.path} exact component={() => <h1>Home</h1>} />
-      <Route
-        path={Routes.UsersManagement.path}
-        exact
-        component={UsersManagement}
-      />
-      <Route path={Routes.Login.path} exact component={Login} />
+      <>
+        {loggedIn ? (
+          <>
+            <Route
+              path={Routes.Home.path}
+              exact
+              component={() => <h1>Home</h1>}
+            />
+            <Route
+              path={Routes.UsersManagement.path}
+              exact
+              component={UsersManagement}
+            />
+            <Route path={Routes.Login.path} exact component={Login} />
+          </>
+        ) : (
+          <>
+            <Redirect to={Routes.Login.path} />
+            <Route path={Routes.Login.path} exact component={Login} />
+          </>
+        )}
+      </>
     </Switch>
   );
 };
